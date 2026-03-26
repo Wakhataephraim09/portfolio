a3377e39d44768254e73127397549ccef38c9a54
wakhataephraim09 <grandyjones09@gmail.com>
2026-03-26 17:33:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Portfolio loaded");
+// SCROLL ANIMATION
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  let scrollY = window.pageYOffset;
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 200;
+
+    if (scrollY > sectionTop) {
+      section.style.opacity = 1;
+      section.style.transform = "translateY(0)";
+    }
+  });
+});
+
+// INITIAL STATE
+sections.forEach(section => {
+  section.style.opacity = 0;
+  section.style.transform = "translateY(50px)";
+  section.style.transition = "all 0.6s ease";
+});
